64676f5ca74a801ad7435ac209dca472affd22d4
yahoneogul <yahoneogul.gmail.com>
2021-02-24 16:59:29 +0900

    pagination, autoSlider, stopSlider 기능 구현

--- js/index/main-slider.js
+++ js/index/main-slider.js
@@ -1,10 +1,12 @@
 "use strict";
 {
+  const slider = document.querySelector(".slider");
   const sliderItems = document.querySelectorAll(".slider-item");
   const pagination = document.querySelector(".pagination");
   const paginationItems = document.querySelectorAll(".pagination-item");
   let targetItem = null;
   let currentItem = null;
+  let timer = null;
   let sliderIndex = 0;
   let sliderCount = 0;
 
@@ -23,10 +25,11 @@
     }
   };
 
-  const fadeOutslider = () => {
+  const fadeOutSlider = (index) => {
     if (currentItem) {
       currentItem.classList.remove("showing");
     }
+    sliderIndex = index;
     sliderIndex = sliderCount % 3;
     sliderItems[sliderIndex].classList.add("showing");
     currentItem = sliderItems[sliderIndex];
@@ -38,11 +41,20 @@
     findTargetItem("pagination-item");
     if (targetItem && targetItem.classList.contains("pagination-item")) {
       const targetIndex = targetItem.dataset.index;
-      sliderIndex = targetIndex;
+      fadeOutSlider(targetIndex);
     }
   };
-  fadeOutslider();
-  setInterval(fadeOutslider, 4000);
+  const autoSlider = () => {
+    timer = setInterval(fadeOutSlider, 4000);
+  };
+  const stopSlider = () => {
+    clearInterval(timer);
+  };
+
   dataIndexSetting(paginationItems);
+  fadeOutSlider(0);
+  autoSlider();
+  slider.addEventListener("mouseover", stopSlider);
+  slider.addEventListener("mouseleave", autoSlider);
   pagination.addEventListener("click", paginationClickHandler);
 }
